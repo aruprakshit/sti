b8ddab5efbdbb095259b7650d4ad0827b3f196c8
Arup Rakshit <aruprakshit@rocketmail.com>
2015-04-09 23:14:32 +0530

    named scope and customized names while using has_many.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :transports, :through => :taggings, source: :vehicle
+  has_many :cars, -> { where(type: 'Car') }, through: :taggings, source: :vehicle
+  has_many :vans, -> { where(type: 'Van') }, through: :taggings, source: :vehicle
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,6 @@
 class Vehicle < ActiveRecord::Base
   has_many :taggings
   has_many :tags, :through => :taggings
+
+  scope :by_type, ->(type) { where(type: type) }
 end
